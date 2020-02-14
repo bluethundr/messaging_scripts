ca917bcfb241cff77a99b7559c7286881352f750
Timothy Dunphy <Timothy.Dunphy@synchronoss.com>
2020-02-14 14:57:38 -0500

    Added my work cell to teh script.

--- send_sms.py
+++ send_sms.py
@@ -6,6 +6,7 @@ account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 
 def send_sms_message():
+    message = ''
     try:
         client = Client(account_sid, auth_token)
     except Exception as e:
@@ -20,6 +21,9 @@ def send_sms_message():
         cell_number = os.environ.get('CELL_PHONE_NUMBER')
     elif recipient == 'Val':
         cell_number = os.environ.get('VAL_PHONE_NUMBER')
+    elif recipient == 'Work':
+        cell_number = os.environ.get('WORK_PHONE_NUMBER')
+
 
     try:
         message = client.messages\
@@ -30,10 +34,14 @@ def send_sms_message():
         print(f"An error has occurred: {e}")
 
     if message:
-        print(f"Message sent: {message.sid}")
+        time.sleep(5)
+        try:
+            print(f"Message sent!\nMessage Sid: {message.sid}\nMessage Status: {message.status}\nMessage Body: {message.body}")
+        except Exception as e:
+            print(f"An error has occurred: {e}")
 
     else:
-        print("Message not sent.")
+        print(f"Message not sent.")
 
 def main():
     send_sms_message()
